f480861077906ab0394c11b7963d06af8ce37f09
wingzerocat <mikechenlou@gmail.com>
2019-03-15 09:57:04 -0500

    changed static endpint

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(morgan('dev'));
-app.use('/:itemId', express.static(path.join(__dirname, 'public')));
+app.use('/pavs/:itemId', express.static(path.join(__dirname, 'public')));
 
 app.use(
   '/api/sizechart',
